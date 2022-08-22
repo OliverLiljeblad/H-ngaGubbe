8df5907df666bd4c1f45f77904726a4827c0b77a
Oliver Liljeblad <liljeblad.oliver@elev.ga.ntig.se>
2022-08-22 11:25:56 +0200

    Gissa spel

--- HängaGubbe/Program.cs
+++ HängaGubbe/Program.cs
@@ -1,10 +1,13 @@
-﻿string rättOrd = "Hej";
+﻿Console.WriteLine("Mata in ett ord");
+string rättOrd = Console.ReadLine();
 
-        Console.Write("Gissa ett ord: ");
-        string ord = Console.ReadLine();
-        if (rättOrd == ord) {
-            Console.WriteLine("Du vann");
-        }
-        else {
-            Console.WriteLine("Du förlorade");
-        }
+Console.Write("Gissa ett ord: ");
+string ord = Console.ReadLine();
+if (rättOrd == ord) {
+    Console.WriteLine("Du vann");
+    }
+    else {
+    Console.WriteLine("Du förlorade");
+    }     
+
+Console.ReadLine();
