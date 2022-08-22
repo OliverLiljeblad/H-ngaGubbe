43531a9d6d7644d29ba87720a4ca61d185619946
Oliver Liljeblad <liljeblad.oliver@elev.ga.ntig.se>
2022-08-22 11:18:57 +0200

    V 0.1

---
+++ HängaGubbe/Program.cs
@@ -0,0 +1,10 @@
+﻿string rättOrd = "Hej";
+
+        Console.Write("Gissa ett ord: ");
+        string ord = Console.ReadLine();
+        if (rättOrd == ord) {
+            Console.WriteLine("Du vann");
+        }
+        else {
+            Console.WriteLine("Du förlorade");
+        }
